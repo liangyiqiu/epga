3be393cedc0e51c5a2a9e63050a04920b78f2bfe
yiqiu <asphaltit@live.com>
2023-07-30 16:38:04 +0800

    add timeout function in spi_listener

--- rtl/spi_listener.v
+++ rtl/spi_listener.v
@@ -1,5 +1,6 @@
 module spi_listener #(
-    parameter first_byte=8'h20
+    parameter first_byte=8'h00,
+    parameter listener_timeout=200
 )
 (
     input clk,
@@ -9,6 +10,8 @@ module spi_listener #(
     output reg spi_listener_interrupt=0
 );
 
+reg timeout;
+reg [15:0] timeout_cnt=0;
 reg [1:0] spi_byte_cnt=0;
 reg [7:0] spi_slave_bytes [0:1];
 
@@ -19,27 +22,52 @@ begin
         case(spi_byte_cnt)
         0:
         begin
-            if(spi_slave_byte[7:5]==first_byte[7:5])
+            if(spi_slave_byte[7:5]==first_byte[7:5] )
             begin
                 spi_slave_bytes[0]<=spi_slave_byte;
                 spi_byte_cnt<=1;
             end
+            else
+                spi_byte_cnt<=0;
         end
         1:
         begin
-            spi_slave_bytes[1]<=spi_slave_byte;
-            spi_byte_cnt<=2;
+            if(timeout)
+                spi_byte_cnt<=0;
+            else
+            begin
+                spi_slave_bytes[1]<=spi_slave_byte;
+                spi_byte_cnt<=2;
+            end
         end
         2:
         begin
-            spi_data<={spi_slave_bytes[0],spi_slave_bytes[1],spi_slave_byte};
-            spi_listener_interrupt<=1;
-            spi_byte_cnt<=0;
+            if(timeout)
+                spi_byte_cnt<=0;
+            else
+            begin
+                spi_data<={spi_slave_bytes[0],spi_slave_bytes[1],spi_slave_byte};
+                spi_listener_interrupt<=1;
+                spi_byte_cnt<=0;
+            end
         end
         endcase
     end
     else
         spi_listener_interrupt<=0;
+end
+
+always @(posedge clk)
+begin
+    if(spi_slave_data_valid)
+    begin
+        timeout<=0;
+        timeout_cnt<=0;
+    end
+    else if(timeout_cnt==listener_timeout)
+        timeout<=1;
+    else
+        timeout_cnt<=timeout_cnt+1;
 end
 
 endmodule
---
+++ sim/tb_spi_listener.v
@@ -0,0 +1,87 @@
+module tb_spi_listener();
+
+reg clk;
+reg spi_slave_data_valid;
+reg [7:0] spi_slave_byte;
+wire [23:0] spi_data;
+wire spi_listener_interrupt;
+wire [15:0] timeout_cnt;
+
+spi_listener uut(
+    .clk(clk),
+    .spi_slave_data_valid(spi_slave_data_valid),
+    .spi_slave_byte(spi_slave_byte),
+    .spi_data(spi_data),
+    .spi_listener_interrupt(spi_listener_interrupt),
+    .timeout_cnt(timeout_cnt)
+);
+
+initial begin
+clk = 0;
+spi_slave_data_valid=0;
+
+#100
+spi_slave_byte=8'ha2;
+spi_slave_data_valid=1;
+#10
+spi_slave_data_valid=0;
+
+#100
+spi_slave_byte=8'hbb;
+spi_slave_data_valid=1;
+#10
+spi_slave_data_valid=0;
+
+#100
+spi_slave_byte=8'hcc;
+spi_slave_data_valid=1;
+#10
+spi_slave_data_valid=0;
+
+#100
+spi_slave_byte=8'h00;
+spi_slave_data_valid=1;
+#10
+spi_slave_data_valid=0;
+
+#100
+spi_slave_byte=8'h20;
+spi_slave_data_valid=1;
+#10
+spi_slave_data_valid=0;
+
+#2000
+spi_slave_byte=8'hcc;
+spi_slave_data_valid=1;
+#10
+spi_slave_data_valid=0;
+#100
+spi_slave_byte=8'h30;
+spi_slave_data_valid=1;
+#10
+spi_slave_data_valid=0;
+
+#100
+spi_slave_byte=8'h20;
+spi_slave_data_valid=1;
+#10
+spi_slave_data_valid=0;
+
+#2000
+spi_slave_byte=8'hbb;
+spi_slave_data_valid=1;
+#10
+spi_slave_data_valid=0;
+
+#100
+spi_slave_byte=8'hcc;
+spi_slave_data_valid=1;
+#10
+spi_slave_data_valid=0;
+
+end
+
+
+always #5 clk=~clk;
+
+endmodule
